bf1893c8c8e35a75e3e1ad2c18aed06c7b909e9b
Bryan Focht <bxfocht@godaddy.com>
2018-11-03 12:43:37 -0700

    update travis build

--- .dev/travis.install-themecheck.sh
+++ .dev/travis.install-themecheck.sh
@@ -6,15 +6,15 @@ export WP_CORE_DIR=/tmp/wordpress
 mkdir -p $WP_CORE_DIR
 cd $WP_CORE_DIR
 
-php /tmp/wp.phar core download --version=$WP_VERSION
-php /tmp/wp.phar config create \
+php /tmp/wp-cli.phar core download --version=$WP_VERSION
+php /tmp/wp-cli.phar config create \
 	--dbname=wordpress \
 	--dbuser=root \
 	--dbpass="" \
 	--skip-check
 
-php /tmp/wp.phar db create
-php /tmp/wp.phar core install \
+php /tmp/wp-cli.phar db create
+php /tmp/wp-cli.phar core install \
 	--url=http://test.dev \
 	--title="WordPress Site" \
 	--admin_user=admin \
@@ -22,8 +22,8 @@ php /tmp/wp.phar core install \
 	--admin_email=admin@tests.dev \
 	--skip-email
 
-php /tmp/wp.phar package install anhskohbo/wp-cli-themecheck
-php /tmp/wp.phar plugin install theme-check --activate
+php /tmp/wp-cli.phar package install anhskohbo/wp-cli-themecheck
+php /tmp/wp-cli.phar plugin install theme-check --activate
 
 export INSTALL_PATH=$WP_CORE_DIR/wp-content/themes/$WP_THEME
 mkdir -p $INSTALL_PATH
